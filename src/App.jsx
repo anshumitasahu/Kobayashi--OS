c3a7e9e9afa222d405bb32db6236c84c4cd61629
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-07-31 23:18:31 +0530

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react';
 import Home from './components/Home';
+import Loading from './components/layout/Loading';
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  const hideLoading = () => {
+    if (isLoading) {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    const timeOutId = setTimeout(hideLoading, 5000)
+    return () => {
+      clearTimeout(timeOutId);
+    }
+  }, [])
+
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      {
+        isLoading && <Loading />
+      }
+      <div>
+        <Home />
+      </div>
+    </>
   )
 }
 
