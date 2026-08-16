7d3280e9e91ee87f578508a4403e5863ff07fa2d
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-08-16 13:34:24 +0530

    updated the indexedDB

--- src/DB/IndexedDB.jsx
+++ src/DB/IndexedDB.jsx
@@ -22,15 +22,37 @@ const openDB = () => {
 
 export const savePhoto = async (blob) => {
     const db = await openDB();
-    const transaction = db.transaction(STORE_NAME, "readwrite");
-    const store = transaction.objectStore(STORE_NAME);
 
-    store.add({
-        image: blob,
-        date: new Date(),
+    return new Promise((resolve, reject) => {
+        const transaction = db.transaction(STORE_NAME, "readwrite");
+        const store = transaction.objectStore(STORE_NAME);
+
+        const request = store.add({
+            image: blob,
+            type: "photo",
+            date: new Date(),
+        });
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
     })
 };
 
+export const saveVideo = async (blob) => {
+    const db = await openDB();
+
+    return new Promise((resolve, reject) => {
+        const transaction = db.transaction(STORE_NAME, "readwrite");
+        const store = transaction.objectStore(STORE_NAME);
+
+        const request = store.add({
+            image: blob,
+            type: "video",
+            date: new Date(),
+        });
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    })
+};
 
 export const getPhotos = async () => {
     const db = await openDB();
