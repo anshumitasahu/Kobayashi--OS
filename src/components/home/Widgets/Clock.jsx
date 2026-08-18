81d4f889eae1bc3dc74270113cbedadf891f1fb5
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-08-18 18:05:36 +0530

    added analog clock instead of number display

--- src/components/home/Widgets/Clock.jsx
+++ src/components/home/Widgets/Clock.jsx
@@ -20,9 +20,38 @@ export default function Clock() {
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
 
+    const hourAngle = (hours % 12) * 30 + minutes * 0.5;
+    const minuteAngle = minutes * 6;
+    const secondAngle = seconds * 6;
+
     return (
         <div>
-            {now}
+            <div className="rounded-full w-40 h-40 bg-amber-100 relative ">
+                <div
+                    className="bg-black w-1 h-10 absolute bottom-[50%] left-[50%] origin-bottom rounded-[50%] -ml-0.75"
+                    style={{
+                        transform: `rotate(${hourAngle}deg)`
+                    }}
+                />
+                <div
+                    className="bg-black w-1 h-15 absolute bottom-[50%] left-[50%] origin-bottom rounded-[50%] -ml-0.75"
+                    style={{
+                        transform: `rotate(${minuteAngle}deg)`
+                    }}
+                />
+                <div
+                    className="bg-black w-0.5 h-15 absolute bottom-[50%] left-[50%] origin-bottom rounded-[50%] -ml-0.75"
+                    style={{
+                        transform: `rotate(${secondAngle}deg)`
+                    }}
+                />
+                <div
+                    className="bg-black w-2 h-2 absolute top-[50%] left-[50%] origin-bottom rounded-full -ml-0.75"
+                    style={{
+                        transform: 'translate(-50%, -50%)'
+                    }}
+                />
+            </div>
         </div>
     )
 }
