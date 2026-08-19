719b9132791ffccb236cfed0c7fdef2df8a325cc
Anshumita Sahu <anshumitasahu.official@gmail.com>
2026-08-19 20:32:18 +0530

    fixed the css of right-click menu

--- src/components/home/RightClick.jsx
+++ src/components/home/RightClick.jsx
@@ -1,19 +1,19 @@
 export default function RightClick() {
     return (
-        <div className="bg-white w-fit px-4 py-2 rounded-sm">
+        <div className="bg-white w-fit rounded-sm">
             <ul className="text-neutral-600 text-sm cursor-default">
                 <li
-                    className="px-2 py-1 hover:bg-gray-100 rounded-sm"
+                    className="px-4 py-2 hover:bg-gray-100 rounded-sm"
                 >
                     Change Widgets
                 </li>
                 <li
-                    className="px-2 py-1 hover:bg-gray-100 rounded-sm"
+                    className="px-4 py-2 hover:bg-gray-100 rounded-sm"
                 >
                     Change Wallpaper
                 </li>
                 <li
-                    className="px-2 py-1 hover:bg-gray-100 rounded-sm"
+                    className="px-4 py-2 hover:bg-gray-100 rounded-sm"
                 >
                     Change Icons
                 </li>
